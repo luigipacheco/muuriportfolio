0d75ff34ac9cb249e38935ba0d63a96ec4d1f613
Luis Arturo Pacheco <luigipach@gmail.com>
2021-02-20 17:39:37 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+// var grid = new Muuri('.grid', {
+//   dragEnabled: true,
+//   layout: {
+//     fillGaps: true
+//   }
+// });
+// var grid1 = new Muuri('.grid1', {
+//   dragEnabled: true,
+//   layout: {
+//     fillGaps: true
+//   }
+// });
+
+var grid1 = new Muuri('.grid-1', {
+  dragEnabled: true,
+  dragContainer: document.body,
+
+});
+
+var grid2 = new Muuri('.grid-2', {
+  dragEnabled: false,
+});
+
+// When all items have loaded refresh their
+// dimensions and layout the grid.
+window.addEventListener('load', function () {
+  grid1.refreshItems().layout();
+  // For a little finishing touch, let's fade in
+  // the images after all them have loaded and
+  // they are corrertly positioned.
+  document.body.classList.add('images-loaded');
+});
+
+
